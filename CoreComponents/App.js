4ab4835f0d3822a36add1e97fa5a06d27e172c06
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-09-22 11:09:27 -0400

    CoreComponents StatusBar

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,34 +1,17 @@
-import { View, Button, Modal, Text } from 'react-native'
-import { useState } from 'react'
+import { View, StatusBar } from 'react-native'
 
 export default function App() {
-	const [isModalVisible, setIsModalVisible] = useState(false);
 
 	return (
 		<View
 			style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}
 		>
-			<Button title='press'
-				onPress={() => setIsModalVisible(true)}
-				color='midnightblue'
-			/>
-			<Modal
-				visible={isModalVisible}
-				onRequestClose={() => setIsModalVisible(false)}
-				animationType='slide'
-				presentationStyle='pageSheet'
+			<StatusBar
+				barStyle='light-content'
+				hidden
 			>
-				<View
-					style={{ flex: 1, backgroundColor: 'lightblue', padding: 60 }}
-				>
-					<Text>Modal Content</Text>
-					<Button
-						color='midnightblue'
-						title='Close'
-						onPress={() => setIsModalVisible(false)}
-					/>
-				</View>
-			</Modal>
+
+			</StatusBar>
 		</View >
 	);
 }
