094e5d061f41d9fdb590c070a78f1f5acc79a1ed
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-09-22 14:20:28 -0400

    StylingRN Multiple Styles

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -1,11 +1,16 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-		<Text>StyleSheet API</Text>
-    </View>
-  );
+	return (
+  		<View style={styles.container}>
+  	  		<View style={[styles.lightblueBg, styles.box]}>
+				<Text>Lightblue box</Text>
+			</View> 	
+			<View style={[styles.lightgreenBg, styles.box]}>
+				<Text>Lightgreen box</Text>
+			</View> 	
+  	  	</View>
+  	);
 }
 
 // exportable for global styles in other Component files
@@ -14,5 +19,16 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: 'plum',
 		padding: 60
-	}
+	},
+	box: {
+		width: 100,
+        height: 100,
+        padding: 10
+	},
+	lightblueBg: { 
+		backgroundColor: 'lightblue',
+	},
+	lightgreenBg: { 
+    	backgroundColor: 'lightgreen',
+    },
 });
